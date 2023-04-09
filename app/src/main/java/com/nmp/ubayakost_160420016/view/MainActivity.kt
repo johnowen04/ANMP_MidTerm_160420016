9a599c813355a4881fa2fcd3d96e6d08b2c5afaf
John Owen <johnowen77777@gmail.com>
2023-04-10 01:12:20 +0700

    Finishing BottomNavigationBar.

--- app/src/main/java/com/nmp/ubayakost_160420016/view/MainActivity.kt
+++ app/src/main/java/com/nmp/ubayakost_160420016/view/MainActivity.kt
@@ -2,11 +2,28 @@ package com.nmp.ubayakost_160420016.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.nmp.ubayakost_160420016.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    // Instantiate navController and initiate later
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initiate navController from supportFragmentManager
+        navController = (supportFragmentManager.findFragmentById(R.id.navHost)
+                as NavHostFragment).navController
+
+        // Bind navController to bottomNav
+        bottomNav.setupWithNavController(navController)
+
+        // Set default menu bottomNav
+        bottomNav.menu.findItem(R.id.itemHome).isChecked = true
     }
 }
