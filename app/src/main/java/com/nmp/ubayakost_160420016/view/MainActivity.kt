1c5037d97c25016774ab7f949bf0855cafd58a77
John Owen <johnowen77777@gmail.com>
2023-04-09 23:58:42 +0700

    Preparing views needed for navigation component.

---
+++ app/src/main/java/com/nmp/ubayakost_160420016/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nmp.ubayakost_160420016.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nmp.ubayakost_160420016.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
