e24a823070998176be548561e42170a8f52c113f
John Owen <johnowen77777@gmail.com>
2023-04-10 15:09:47 +0700

    Updating register process. Updating UserViewModel.kt and adding register business logic in view model.

--- app/src/main/java/com/nmp/ubayakost_160420016/view/RegisterFragment.kt
+++ app/src/main/java/com/nmp/ubayakost_160420016/view/RegisterFragment.kt
@@ -5,14 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.nmp.ubayakost_160420016.R
+import com.nmp.ubayakost_160420016.viewmodel.UserViewModel
+import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
+    private lateinit var viewModel: UserViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        activity?.onBackPressedDispatcher?.addCallback(requireActivity(), object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val action = RegisterFragmentDirections.actionRegisterToLogin()
+                Navigation.findNavController(view).navigate(action)
+            }
+        })
+
+        viewModel = ViewModelProvider(this)[UserViewModel::class.java]
+
+        btnCreateAccRegister.setOnClickListener {
+            val username = editUsernameRegister.text.toString()
+            val password = editPasswordRegister.text.toString()
+            val repeatPassword = editPasswordRepeatRegister.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty() && repeatPassword.isNotEmpty()) {
+                if (username.length < 9) {
+                    Toast.makeText(requireContext(), "Enter at least 9 characters for username", Toast.LENGTH_SHORT).show()
+                } else if (password.length < 8 || repeatPassword.length < 8) {
+                    Toast.makeText(requireContext(), "Enter at least 8 characters for password and repeat password", Toast.LENGTH_SHORT).show()
+                } else {
+                    if (password == repeatPassword) {
+                        viewModel.register(username, password) { success, message ->
+                            if (success) {
+                                val action = RegisterFragmentDirections.actionRegisterToLogin()
+                                Navigation.findNavController(view).navigate(action)
+                            } else {
+                                Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    } else {
+                        Toast.makeText(requireContext(), "Repeat password correctly", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }  else {
+                Toast.makeText(requireContext(), "Username or password cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/nmp/ubayakost_160420016/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/nmp/ubayakost_160420016/viewmodel/UserViewModel.kt
@@ -2,8 +2,11 @@ package com.nmp.ubayakost_160420016.viewmodel
 
 import android.app.Application
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.navigation.Navigation
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -11,6 +14,7 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.nmp.ubayakost_160420016.model.User
 import com.nmp.ubayakost_160420016.util.SharedPreferencesProvider
+import com.nmp.ubayakost_160420016.view.RegisterFragmentDirections
 import org.json.JSONObject
 
 class UserViewModel(application: Application): AndroidViewModel(application) {
@@ -60,4 +64,36 @@ class UserViewModel(application: Application): AndroidViewModel(application) {
 
     // Check if user isLoggedOn or not
     fun isLogin(): Boolean = sharedPreferences.isLogin()
+
+    // Register function - Used to register new user
+    fun register(username: String, password: String, onSuccess: (success: Boolean, message: String?) -> Unit) {
+        queue = Volley.newRequestQueue(getApplication())
+        val url = "http://10.0.2.2/anmp/ubayakost_api/register.php"
+        val stringRequest = object: StringRequest(
+            Method.POST, url,
+            {
+                Log.d("showvolley", it)
+                val obj = JSONObject(it)
+
+                if (obj.getString("result") == "success") {
+                    onSuccess(true, obj.getString("message"))
+                } else {
+                    onSuccess(false, obj.getString("message"))
+                }
+            },
+            {
+                Log.e("showvolley", it.toString())
+            }
+        ) {
+            override fun getParams(): MutableMap<String, String>? {
+                return mutableMapOf(
+                    "username" to username,
+                    "password" to password
+                )
+            }
+        }
+        stringRequest.apply { tag = TAG }
+
+        queue?.add(stringRequest)
+    }
 }
